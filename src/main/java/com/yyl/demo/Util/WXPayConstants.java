b081011c97a9369ff3d7244de584f66f0b9a52e7
yuyuelong <yuyuelong28@163.com>
2018-05-15 11:00:21 +0800

    微信支付常量

---
+++ src/main/java/com/yyl/demo/Util/WXPayConstants.java
@@ -0,0 +1,15 @@
+package com.yyl.demo.Util;
+
+/**
+ * 微信支付常量</br>
+ */
+public interface WXPayConstants {
+    /**应用编号**/
+    String APP_ID = "商户APPID";
+
+    /**商户号**/
+    String MCH_ID = "商户号";
+
+    /**API秘钥**/
+    String API_KEY = "密钥";
+}
